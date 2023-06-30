e4756f461b5da92093f60e8c0c2f68390a84d525
abc <abc@Anagha>
2023-06-30 20:15:50 +0530

    Created upload and drag and drop packages

---
+++ src/uploadanddownloadexample/UploadFileExample.java
@@ -0,0 +1,11 @@
+package uploadanddownloadexample;
+
+public class UploadFileExample {
+
+	public static void main(String[] args) {
+		//refer this website for concept: https://www.softwaretestinghelp.com/file-upload-in-selenium/
+		//refer this website as demo website: https://demo.guru99.com/test/upload/ 
+
+	}
+
+}
